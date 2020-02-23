c9603de9df7b0594f47fdedd20125556c5c0dded
Marco Rossi <marco.rossi4@fcagroup.com>
2020-02-23 17:33:41 +0100

    feat: Add spline.h for generating spline and continue lane keep implementation

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
+#include "spline.h"
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -101,14 +102,89 @@ int main()
            * TODO: define a path made up of (x,y) points that the car will visit
            *   sequentially every .02 seconds
            */
-          double dist_inc = 0.3;
-          for (int i = 0; i < 50; ++i)
+
+          // double dist_inc = 0.3;
+          // for (int i = 0; i < 50; ++i)
+          // {
+          //   double next_s{car_s + (i + 1) * dist_inc};
+          //   double next_d = 10;
+          //   vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          //   next_x_vals.push_back(xy[0]);
+          //   next_y_vals.push_back(xy[1]);
+          // }
+
+          int lane = 1;
+          double ref_vel{49.5}; //[MpH]
+          vector<double> ptsx;
+          vector<double> ptsy;
+          double ref_x{car_x};
+          double ref_y{car_y};
+          double ref_yaw{deg2rad(car_yaw)};
+
+          //Here I want to find the last couple of points that the car was following to genereate a smooth transition
+          //First initialization step
+          if (previous_path_x.size() < 2)
           {
-            double next_s{car_s + (i + 1) * dist_inc};
-            double next_d = 6;
-            vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
-            next_x_vals.push_back(xy[0]);
-            next_y_vals.push_back(xy[1]);
+            double prev_car_x{car_x - cos(car_yaw)};
+            double prev_car_y{car_y - sin(car_yaw)};
+
+            ptsx.push_back(prev_car_x);
+            ptsx.push_back(car_x);
+            ptsy.push_back(prev_car_y);
+            ptsy.push_back(car_y);
+          }
+          //All other cases
+          else
+          {
+            ref_x = previous_path_x[previous_path_x.size() - 1];
+            ref_y = previous_path_y[previous_path_y.size() - 1];
+
+            double ref_x_prev{previous_path_x[previous_path_x.size() - 2]};
+            double ref_y_prev{previous_path_y[previous_path_y.size() - 2]};
+            ref_yaw = (ref_y - ref_y_prev) / (ref_x - ref_x_prev);
+
+            ptsx.push_back(ref_x_prev);
+            ptsx.push_back(ref_x);
+            ptsy.push_back(ref_y_prev);
+            ptsy.push_back(ref_y);
+          }
+
+          //Now I create the real new trajectory that the car has to follow
+          //3 points equally spaced of 30 meters are created
+
+          vector<double> next_wp0{getXY(car_s + 30, 2 + 4 * lane, map_waypoints_s, map_waypoints_x, map_waypoints_y)};
+          vector<double> next_wp1{getXY(car_s + 60, 2 + 4 * lane, map_waypoints_s, map_waypoints_x, map_waypoints_y)};
+          vector<double> next_wp2{getXY(car_s + 90, 2 + 4 * lane, map_waypoints_s, map_waypoints_x, map_waypoints_y)};
+
+          ptsx.push_back(next_wp0[0]);
+          ptsx.push_back(next_wp1[0]);
+          ptsx.push_back(next_wp2[0]);
+
+          ptsy.push_back(next_wp0[1]);
+          ptsy.push_back(next_wp1[1]);
+          ptsy.push_back(next_wp2[1]);
+
+          //Here I shift the car reference angle to zero
+          for (int i = 0; i < ptsx.size(); i++)
+          {
+            double shift_x = ptsx[i] - ref_x;
+            double shift_y = ptsy[i] - ref_y;
+
+            ptsx[i] = (shift_x * cos(0 - ref_yaw) - shift_y * sin(0 - ref_yaw));
+            ptsy[i] = (shift_x * sin(0 - ref_yaw) + shift_y * cos(0 - ref_yaw));
+          }
+
+          //Create the spline that we will use to interpolate those points
+          tk::spline s;
+
+          s.set_points(ptsx, ptsy);
+
+          //First, we want to feed the next_vals vectors with the part of the path
+          //that was not sent to the simulator during the previous iteration
+          for (int i = 0; i < previous_path_x.size(); i++)
+          {
+            next_x_vals.push_back(previous_path_x[i]);
+            next_y_vals.push_back(previous_path_y[i]);
           }
 
           msgJson["next_x"] = next_x_vals;
